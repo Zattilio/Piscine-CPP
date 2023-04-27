606082eb8daa252b82d772586a4cc0ecd2092926
Joseph LANZA <jlanza@student.42.fr>
2023-04-27 17:24:47 +0200

    phone book in progress

--- CPP00/ex01/PhoneBook.class.cpp
+++ CPP00/ex01/PhoneBook.class.cpp
@@ -6,7 +6,7 @@
 /*   By: jlanza <jlanza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:01:44 by jlanza            #+#    #+#             */
-/*   Updated: 2023/04/25 18:28:12 by jlanza           ###   ########.fr       */
+/*   Updated: 2023/04/27 16:22:45 by jlanza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 PhoneBook::PhoneBook(void)
 {
-	this->index = 0;
+	this->index = -1;
 	return;
 }
 
@@ -23,7 +23,7 @@ PhoneBook::~PhoneBook(void) {}
 int	PhoneBook::add(void)
 {
 	this->index = (this->index + 1) % 8;
-	std::cout << "Enter first name:" << std::endl;
+	std::cout << std::endl << "Enter first name:" << std::endl;
 	std::cin >> this->contact[this->index].first_name;
 	if (std::cin.eof())
 		return (-1);
@@ -46,8 +46,35 @@ int	PhoneBook::add(void)
 	return (0);
 }
 
+static void	print_shortened(std::string str)
+{
+	if (str.size() <= 10)
+	{
+		std::cout << str;
+		for (unsigned long index = 0; index < 10 - str.size(); index++)
+			std::cout << " ";
+	}
+	else
+		std::cout << str.substr(0, 9) << ".";
+}
+
 void	PhoneBook::search(void)
 {
+	std::cout << std::endl << "first name|last name |nickname  |phone nbr.|darkest secret";
+	std::cout << std::endl;
+	for (int i = 0; i < 8; i++)
+	{
+		print_shortened(this->contact[i].first_name);
+		std::cout << "|";
+		print_shortened(this->contact[i].last_name);
+		std::cout << "|";
+		print_shortened(this->contact[i].nickname);
+		std::cout << "|";
+		print_shortened(this->contact[i].phone_number);
+		std::cout << "|";
+		print_shortened(this->contact[i].darkest_secret);
+		std::cout << std::endl;
+	}
 	return;
 }
 
--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jlanza <jlanza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:11:05 by jlanza            #+#    #+#             */
-/*   Updated: 2023/04/25 18:26:27 by jlanza           ###   ########.fr       */
+/*   Updated: 2023/04/27 16:18:40 by jlanza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,26 @@ int	main(int argc, char *argv[])
 	std::string	buf;
 	PhoneBook	book;
 
+	(void)argv;
+	if (argc > 1)
+	{
+		std::cerr << "Too many arguments.";
+		return (1);
+	}
+	std::cout << "Enter command:" << std::endl;
 	while (1)
 	{
 		std::cin >> buf;
 		if (std::cin.eof())
 			return (1);
-		if (buf == "ADD")
+		else if (buf == "ADD")
 			book.add();
-		if (buf == "SEARCH")
+		else if (buf == "SEARCH")
 			book.search();
-		if (buf == "EXIT")
+		else if (buf == "EXIT")
 			return (0);
+		else
+			std::cout << "Invalid command: use ADD, SEARCH or EXIT" << std::endl;
+		std::cout << std::endl << "Enter command:" << std::endl;
 	}
-	(void)argc;
-	(void)argv;
 }
